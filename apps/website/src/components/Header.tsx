348a148f46b61740d743a411f2d47682b9adce39
Jenil Desai <jenildev91@gmail.com>
2025-03-08 20:09:28 +0530

    v1.4.1
    + Fixed Sign Up Issues.

--- apps/website/src/components/Header.tsx
+++ apps/website/src/components/Header.tsx
@@ -1,13 +1,13 @@
 "use client";
-import { useState } from "react";
-import Link from "next/link";
-
-import { Button } from "@/components/ui/button";
-import { Menu, X } from "lucide-react";
-import { HEADER_LINKS } from "../constants/headerLinks";
-import Image from "next/image";
 import { useAuth, UserButton } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { HEADER_LINKS } from "../constants/headerLinks";
+import { Button } from "@/components/ui/button";
 
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -18,7 +18,7 @@ export default function Header() {
     <header className="sticky top-0 z-50 w-full border-b border-black/10 bg-white/80 backdrop-blur supports-[backdrop-filter]:bg-white/60">
       <div className="flex h-16 items-center justify-around">
         <div className="flex items-center gap-2">
-          <Image src={"/icons/icon-512.png"} width={45} height={45} alt="Icon" />
+          <Image src={"/icons/icon-512.png"} width={30} height={30} alt="Icon" />
           <span className="text-xl font-bold text-black">PixelBloom Studio</span>
         </div>
 
@@ -33,7 +33,9 @@ export default function Header() {
         </nav>
 
         {isSignedIn ? (
-          <UserButton />
+          <div className="hidden md:flex items-center gap-4">
+            <UserButton showName appearance={{ layout: { animations: true } }} />
+          </div>
         ) : (
           <div className="hidden md:flex items-center gap-4">
             <Button onClick={() => router.push("/sign-in")} variant="outline" size="sm" className="bg-transparent border-black/20 text-black hover:bg-black/10 hover:text-black">
@@ -55,13 +57,15 @@ export default function Header() {
           <nav className="flex flex-col space-y-4">
             {HEADER_LINKS.map((headerLink, idx) => {
               return (
-                <Link key={idx} href={headerLink.href} className="text-sm font-medium text-black/80 hover:text-black">
+                <Link key={idx} href={headerLink.href} className="text-sm font-medium text-black/80 hover:text-black text-center bg-black/10 p-2 rounded-md hover:bg-black/20">
                   {headerLink.title}
                 </Link>
               );
             })}
             {isSignedIn ? (
-              <UserButton />
+              <div className="flex justify-center items-center gap-4">
+                <UserButton showName />
+              </div>
             ) : (
               <div className="flex flex-col space-y-2 pt-2">
                 <Button onClick={() => router.push("/sign-in")} variant="outline" size="sm" className="w-full bg-transparent border-black/20 text-black hover:bg-black/10 hover:text-black">
