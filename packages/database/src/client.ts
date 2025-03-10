8c68b060548ff5bcf801d9101d0fa0281748597f
Jenil Desai <jenildev91@gmail.com>
2025-03-10 20:29:04 +0530

    v1.4.14
    + Fixed Prisma issues

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,8 +1,18 @@
-// lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+declare global {
+  // eslint-disable-next-line no-var
+  var cachedPrisma: PrismaClient;
+}
 
-export const db = globalForPrisma.prisma || new PrismaClient();
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  prisma = global.cachedPrisma;
+}
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+export const db = prisma;
