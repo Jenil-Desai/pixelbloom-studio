e8955dfbffdf22e2ce56ce693086668e2aff43a8
Jenil Desai <jenildev91@gmail.com>
2025-03-11 23:56:35 +0530

    v1.5.1
    + Fixed Prisma Engine Issue with Next.js + Monorepo.

---
+++ apps/website/next.config.js
@@ -0,0 +1,46 @@
+// import { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "images.unsplash.com",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "i.pravatar.cc",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+// next.config.js
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
